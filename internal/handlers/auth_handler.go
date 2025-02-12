44ddeed9dba9d98387044a1a6d0fee4f92086d21
Sergey <pav220521@gmail.com>
2025-02-12 22:29:12 +0300

    fifth

---
+++ internal/handlers/auth_handler.go
@@ -0,0 +1,58 @@
+// Обработка регистрации и авторизации.
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/1SergPav1/notes_api/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+type AuthHandler struct {
+	AuthService *service.AuthService
+}
+
+func NewAuthHandler(authService *service.AuthService) *AuthHandler {
+	return &AuthHandler{authService}
+}
+
+// Регистрация пользователя
+func (h *AuthHandler) Register(c *gin.Context) {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Неккоректные данные"})
+		return
+	}
+
+	err := h.AuthService.Register(req.Username, req.Password)
+	if err != nil {
+		c.JSON(http.StatusConflict, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Пользователь зарегистрирован"})
+}
+
+func (h *AuthHandler) Login(c *gin.Context) {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Неккоректные данные"})
+		return
+	}
+
+	token, err := h.AuthService.Login(req.Username, req.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
